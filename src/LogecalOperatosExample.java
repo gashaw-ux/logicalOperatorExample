1bf74cb66e9f207990040af3ed4763d49f77ac8c
kal01aki <kal01aki@users.noreply.github.com>
2022-10-26 17:58:33 +0300

    ANDOR

--- src/LogecalOperatosExample.java
+++ src/LogecalOperatosExample.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class LogecalOperatosExample {
+public class LogecalOperatosExample {
+    public static void main(String[] args) {
+// Voting Scenario
+
+        boolean isUnderage = false;
+        boolean isEthiopian = true;
+        boolean hasAnID = true;
+        boolean isPreviousVoter = false;
+        boolean criminalRecord =true;
+
+        boolean canVote = !isUnderage && isEthiopian && ((hasAnID || isPreviousVoter) || (hasAnID || criminalRecord) || (isPreviousVoter || criminalRecord));
+
+        System.out.println(canVote);
+    }
 }
